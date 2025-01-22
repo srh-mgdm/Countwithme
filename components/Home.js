092565765742f0a665e11f92960d2114f6cb9f52
Sareh <moghaddam_sareh@yahoo.com>
2025-01-22 17:06:28 +0100

    create a simple counter

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,24 @@
 import styles from '../styles/Home.module.css';
+import { useState } from 'react';
 
 function Home() {
+  const [count, setCount] = useState(0);
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
-    </div>
+    <>
+     <div className={styles.main}>
+       <h1 className={styles.title}>
+         count with me
+       </h1>
+       <div className={styles.buttonContainer}>
+         <button onClick={() => setCount(count + 1)} className={styles.buttonPlus}>+</button>
+         <span className={styles.count}>{count}</span>
+         <button onClick={() => count>0 && setCount(count - 1)} className={styles.buttonMinus} disabled={count===0}>-</button>
+       </div>
+       
+     </div>
+       
+      
+    </>
   );
 }
 
